8cdf056441d039f62f8e2e17c2d4b0d5b736ea3f
Yasushi SHOJI <yashi@spacecubics.com>
2023-07-24 01:14:55 +0900

    zephyr: Add env_zephyr.c
    Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>

---
+++ src/env_zephyr.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+
+#include "rcutils/env.h"
+
+bool rcutils_set_env(const char * env_name, const char * env_value)
+{
+	return false;
+}
+
+const char* rcutils_get_env(const char * env_name, const char ** env_value)
+{
+	if (NULL != env_value) {
+		*env_value = "";
+		return NULL;
+	} else {
+		return "Invalid arguemnt";
+	}
+}
+
+const char* rcutils_get_home_dir(void)
+{
+	return NULL;
+}
